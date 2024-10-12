5de7c4102e625ad485938844ea2dcd7739737301
JPW03 <03.jamie.w@googlemail.com>
2024-10-12 09:38:43 +0100

    Lazy, colourschemes and telescope

---
+++ init.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+require("config.lazy")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,30 @@
+return {
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_background = 'hard'
+        vim.g.everforest_enable_italic = true
+        vim.cmd.colorscheme('everforest')
+      end
+    },
+    {
+        "samharju/synthweave.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme("synthweave")
+            -- transparent version
+            -- vim.cmd.colorscheme("synthweave-transparent")
+        end
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+    }
+}
