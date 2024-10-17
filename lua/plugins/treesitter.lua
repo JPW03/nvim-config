78c2b9228b3cc5fcba57e9d6a25935ec159fd755
JPW03 <03.jamie.w@googlemail.com>
2024-10-17 15:58:54 +0100

    Treesitter and neo-tree

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build=":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = "all",
+                auto_install = true, -- Note: tree-sitter-cli should be installed (npm install --global tree-sitter-cli)
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    }
+}
