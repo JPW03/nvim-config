e3c633cd23536dc92cba52f8ea96535324d602aa
JPW03 <03.jamie.w@googlemail.com>
2024-12-05 09:56:37 +0000

    LazyGit Custom Terminal and formatting

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,30 +1,34 @@
 return {
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-    {
-      'sainnhe/everforest',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        vim.g.everforest_background = 'hard'
-        vim.g.everforest_enable_italic = true
-        vim.cmd.colorscheme('everforest')
-      end
-    },
-    {
-        "samharju/synthweave.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme("synthweave")
-            -- transparent version
-            -- vim.cmd.colorscheme("synthweave-transparent")
-        end
-    },
-    {
-        "scottmckendry/cyberdream.nvim",
-        lazy = false,
-        priority = 1000,
-    }
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.everforest_background = 'hard'
+			vim.g.everforest_enable_italic = true
+			vim.cmd.colorscheme("everforest")
+            -- TODO this is the only colourscheme I've managed to get working with treesitter, although its not as green as I thought
+		end,
+	},
+	{
+		"samharju/synthweave.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme("synthweave")
+			-- transparent version
+			-- vim.cmd.colorscheme("synthweave-transparent")
+		end,
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme("cyberdream")
+		end,
+	},
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,7 +7,30 @@ return {
             toggleterm.setup({})
             -- TODO setup keymap for this or just use :terminal
             -- TODO also fix the terminal dark blue colour, its far too difficult to see
+
+            -- Custom terminals
+            -- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#custom-terminals
+            local terminal = require('toggleterm.terminal').Terminal
+            local lazygit = terminal:new({
+                cmd = 'lazygit',
+                direction = 'float',
+                float_opts = {
+                    border = 'curved',
+                },
+                on_open = function ()
+                    vim.cmd('startinsert') -- start in terminal mode not normal mode
+                    -- TODO fix opening lazygit for after the first time in the same session, doesn't start in terminal mode
+                end,
+                hidden = true
+            })
+
+            -- Keymaps
+            vim.keymap.set('n', '<leader>git', function ()
+                lazygit:toggle()
+            end)
+            vim.keymap.set('n', '<leader>t', ':ToggleTerm<CR>')
+            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]]) -- Switch window in terminal mode
+            vim.keymap.set('t', '<leader><esc>', [[<C-\><C-n>:q<CR>]]) -- Close terminal from terminal mode
         end,
     }
-    -- This in combination with LazyGit is my Git integration
 }
